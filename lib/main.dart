ac8e81ccd962c7559abc8068ffb8f707ecbdda00
Ashik <ashik@disystem.net>
2020-10-15 16:09:09 +0600

    background

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_background_location/flutter_background_location.dart';
+import 'package:location/location.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -10,10 +12,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home:  MyHomePage(title: 'Flutter Location Demo'),
+
     );
   }
 }
@@ -37,10 +40,20 @@ class _MyHomePageState extends State<MyHomePage> {
 
   int count = 0;
 
+  final Location location = Location();
+  bool _serviceEnabled;
+
   @override
   void initState() {
     super.initState();
 
+
+    _checkService();
+
+  }
+
+
+  void myLocation(){
     FlutterBackgroundLocation.startLocationService();
     FlutterBackgroundLocation.getLocationUpdates((location) {
       setState(() {
@@ -64,6 +77,96 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+
+
+  Future<void> _checkService() async {
+    final bool serviceEnabledResult = await location.serviceEnabled();
+    setState(() {
+      _serviceEnabled = serviceEnabledResult;
+      print(_serviceEnabled);
+    });
+
+    if(_serviceEnabled){
+      myLocation();
+    }else{
+      _requestService();
+    }
+
+
+  }
+
+  Future<void> _requestService() async {
+    if (_serviceEnabled == null || !_serviceEnabled) {
+      final bool serviceRequestedResult = await location.requestService();
+      setState(() {
+        _serviceEnabled = serviceRequestedResult;
+      });
+
+      print(_serviceEnabled);
+
+
+      if (!serviceRequestedResult) {
+        return;
+      }
+      else{
+        myLocation();
+      }
+    }
+  }
+
+
+
+  // void locationService() async{
+  //
+  // }
+  // void start_Loc() async {
+  //   if (await _checkLocationPermission()) {
+  //     await locationService();
+  //   } else {
+  //     print('elseee');
+  //     PermissionStatus permission =
+  //         await LocationPermissions().requestPermissions();
+  //     print(permission);
+  //
+  //     //PermissionStatus permission = await LocationPermissions().checkPermissionStatus();
+  //
+  //   }
+  // }
+  // void openLocationSetting() async {
+  //   final AndroidIntent intent = new AndroidIntent(
+  //     action: 'android.settings.LOCATION_SOURCE_SETTINGS',
+  //   );
+  //   await intent.launch();
+  // }
+  // Future<bool> _checkLocationPermission() async {
+  //   final access = await LocationPermissions().checkPermissionStatus();
+  //   switch (access) {
+  //     case PermissionStatus.unknown:
+  //     case PermissionStatus.denied:
+  //       //do whn permission denied
+  //
+  //     case PermissionStatus.restricted:
+  //       final permission = await LocationPermissions().requestPermissions(
+  //         permissionLevel: LocationPermissionLevel.locationAlways,
+  //       );
+  //       if (permission == PermissionStatus.granted) {
+  //         return true;
+  //       } else {
+  //         return false;
+  //       }
+  //       break;
+  //     case PermissionStatus.granted:
+  //       return true;
+  //       break;
+  //     default:
+  //       return false;
+  //       break;
+  //   }
+  // }
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -76,7 +179,9 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Text(
               "$count\n\n\n$latitude\n$longitude",
-              style: TextStyle(fontSize: 14, ),
+              style: TextStyle(
+                fontSize: 14,
+              ),
             ),
           ],
         ),
@@ -84,4 +189,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
